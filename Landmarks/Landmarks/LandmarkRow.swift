ee82799dbab66931b40d5ddc1afadfb4addca3b4
ThinhLe <thinhlefrs@gmail.com>
2020-05-07 22:08:14 +0700

    Config Landmark Row

---
+++ Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by ThinhLe on 5/7/20.
+//  Copyright © 2020 ThinhLe. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12))
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
