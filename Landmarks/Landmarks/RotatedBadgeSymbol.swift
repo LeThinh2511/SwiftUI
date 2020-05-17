92464d4df30a319de2f7bb356b57022c64fae21a
Thinh Le <thinhlefrs@gmail.com>
2020-05-17 21:33:09 +0700

    Drawing paths and shapes

---
+++ Landmarks/Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Thinh Le on 5/17/20.
+//  Copyright © 2020 ThinhLe. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+        .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
