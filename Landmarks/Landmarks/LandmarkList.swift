0e136944e0068f8eeb12065c1e66b94876acd4c8
Thinh Le <thinhlefrs@gmail.com>
2020-05-17 22:14:34 +0700

    composing complex interfaces

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -12,27 +12,27 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Show Favorites only")
-                }
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Show Favorites only")
+            }
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-        .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-        .environmentObject(UserData())
+        NavigationView {
+            LandmarkList()
+                .environmentObject(UserData())
+        }
+        
     }
 }
